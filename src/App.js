afcb8a07e75f6e8ac93bad41fae9c179765c3f70
Noor alqaoud <annoia09@gmail.com>
2024-11-19 20:33:42 +0300

    “chocolateshop”

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import storeImage from "./images/chocolatestore.png";
+import darkchocolateImage from "./images/dark chocolate.jpg";
+import milkchocolateImage from "./images/milk chocolate.jpg";
+import whitechocolateImage from"./images/whitechocolate.jpg";
 
-function App() {
+function App(){
+  const products =[{
+    name:"Dark Chocolate",
+    price:"4 KWD",
+    image: darkchocolateImage,
+    },
+    {
+    name:"Milk Chocolate",
+    price:"5 KWD",
+    image: milkchocolateImage,
+    },
+    {
+    name:"White Chocolate",
+    price:"3 KWD",
+    image: whitechocolateImage,
+    }
+  ];
+
+  const productList=products.map((product,index) => {
+    return(
+      <div key={index}>
+        <img className="product-Image" src={product.image} alt={product.name} />
+        <p>{product.name}</p>
+        <p>{product.price}</p>
+      </div>
+    );
+  });
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the Chocolate Shop</h1>     
+      <p> Your One stop destination for the finest belgian chocolates</p>
+      <img className="storeImage"
+      src={storeImage} alt="chocolate"></img>
+     <div className="Product-List">{productList}</div>
     </div>
   );
 }
